62129cfaef63be5480621ab09fda99df1c5e8c04
williamellings <wille.ellings@hotmail.com>
2025-10-06 21:55:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Temperaturkontroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Här kan vi kolla in tempen mata in tempen så får du ett svar: ");
+            string input = Console.ReadLine();
+
+            Thermometer thermometer = new Thermometer();
+            thermometer.CheckTempretaure();
+
+            
+        }
+    }
+}
---
+++ Thermometer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperaturkontroll
+{
+    internal class Thermometer
+    {
+        public double Temprature { get; set; }
+
+        public void CheckTempretaure()
+        {
+            
+
+            if (Temprature < 0)
+            {
+                Console.WriteLine("Det är minus grader");
+                
+            }
+            else if (Temprature <= 30)
+            {
+                Console.WriteLine("Det är normal tempratur");
+
+            }
+            else
+            {
+                Console.WriteLine("Det är flr varmnt");
+
+            }
+    
+
+        }
+    }   
+
+}
